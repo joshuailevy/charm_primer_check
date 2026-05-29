c3b1552684bbc83828840538b86fd7574439582b
joshuailevy <joshua.levy16@gmail.com>
2026-05-28 17:44:18 -0700

    now for more pathogens/primers

--- src/check_primers_and_probes.py
+++ src/check_primers_and_probes.py
@@ -49,149 +49,179 @@ def gunzip_if_needed(path):
 
     return str(unzipped_path)
 
-
-genomes = gunzip_if_needed('../sequences/Parainfluenza_virus_1/Parainfluenza_virus_1.fasta.gz')
-metadata_path = '../sequences/Parainfluenza_virus_1/Parainfluenza_virus_1_metadata.csv'
-# old_fp = "CGAGCGAGTCGATTTATTACCA"
-forward_primer = "CGGGCGAGYMGATTTATTACCA"
-reverse_primer = "CAATCCGGTTAACATAATTTGT"
-probe = "AATATGGCATTAAAAGARGCAGGW"
-
+primer_info = {'Parainfluenza_virus_1':{'forward_primer':"CGGGCGAGYMGATTTATTACCA",'reverse_primer':"CAATCCGGTTAACATAATTTGT",'probe':"AATATGGCATTAAAAGARGCAGGW",'assay':1},
+               'Parainfluenza_virus_2':{'forward_primer':"AGGACTATGAAAACCATTTACCTAAGTGA",'reverse_primer':"AAGCAAGTCTCAGTTCAGCTAGRTCA",'probe':"ATCAATCGVAAAAGCTGTTCAGTCACTGCTATAC",'assay':1},
+               'Parainfluenza_virus_3':{'forward_primer':"CCRTCTGTTGGACCAGGDATA",'reverse_primer':"GTGTTRCAGATTGCATTCTCATTTA",'probe':"TACAAAGGCAAAATAATATTTCTYGGGTATGGAGGT",'assay':1},
+               'Parainfluenza_virus_4':{'forward_primer':"CTTTTCGACGTGAAGTAGTATTAGA",'reverse_primer':"AGTAATCAGTTGATCGTTGGATGTG",'probe':"ACTCAAGTTAGATCTTTGACTCCTCT",'assay':1},
+               'Parainfluenza_virus_4':{'forward_primer':"CTTTTCGACGTGARGTAGTTCTAGA",'reverse_primer':"AGTAATCATTTGACCGTTGGATRTG",'probe':"ACTCAGGTYAGATCWTTGACTCCTCT",'assay':2},
+               'Human_Metapneumovirus':{'forward_primer':"CAAGTGCGACATTGATGACCTRAA",'reverse_primer':"ATTGCCGCACAACATTYAGAAA",'probe':"TGGCYGTTAGYTTCAGTCARTTCAACAGA",'assay':1},
+               'Human_Metapneumovirus':{'forward_primer':"CAAATGTGACATTGCTGATYTRAA",'reverse_primer':"ACTGCCGCACAACATTTARRAAT",'probe':"TGGCTGTCAGCTTCAGTCARTTCAACAGA",'assay':2},
+    }
 ambiguous_bases = {'R', 'Y', 'S', 'W',
                     'K', 'M', 'B', 'D',
                     'H', 'V', 'N'}
 ## expanding ambiguous bases to multiple primers in the ambiguous set
-translator = {'Y':['C','T'],'W':['A','T'],'M':['A','C'],'R':['A','G']}
-
-primer_df = pd.DataFrame(
-    [
-        [primer_left, primer_right]
-        for primer_left in expand_primer_options(forward_primer, translator)
-        for primer_right in expand_primer_options(reverse_primer, translator)
-    ],
-    columns=["primer_seq_x", "primer_seq_y"],
-)
-
-metadata_df = pd.read_csv(
-    metadata_path,
-    usecols=["accession", "collection_date", "geo_loc_name"],
-).set_index("accession")
-
+translator = {'Y': ['C', 'T'],
+                'W': ['A', 'T'],
+                'M': ['A', 'C'],
+                'R': ['A', 'G'],
+                'S': ['G', 'C'],
+                'K': ['G', 'T'],
+                'B': ['C', 'G', 'T'],
+                'D': ['A', 'G', 'T'],
+                'H': ['A', 'C', 'T'],
+                'V': ['A', 'C', 'G'],
+                'N': ['A', 'C', 'G', 'T']}
 maxmismatch=3
-all_results = []
+for virus in primer_info.keys():
+    print(f"Working on {virus}")
+    genomes = gunzip_if_needed(f'../sequences/{virus}/{virus}.fasta.gz')
+    metadata_path = f'../sequences/{virus}/{virus}_metadata.csv'
+    forward_primer = primer_info[virus]['forward_primer']
+    reverse_primer = primer_info[virus]['reverse_primer']
 
-for _, row in primer_df.iterrows():
-    primer_left = row["primer_seq_x"]
-    primer_right = row["primer_seq_y"]
-    primer_right = str(Seq(primer_right).reverse_complement())
-    
-    pattern_left = f"({primer_left}){{s<={maxmismatch}}}"
-    pattern_right = f"({primer_right}){{s<={maxmismatch}}}"
+    primer_df = pd.DataFrame(
+        [
+            [primer_left, primer_right]
+            for primer_left in expand_primer_options(forward_primer, translator)
+            for primer_right in expand_primer_options(reverse_primer, translator)
+        ],
+        columns=["primer_seq_x", "primer_seq_y"],
+    )
 
-    # # Parse sequences in the multifasta
-    for genome_record in SeqIO.parse(genomes, "fasta"):
-        genome_id = genome_record.id
-        genome_seq = str(genome_record.seq)
-        genome_metadata = metadata_df.loc[genome_id]
-        collection_date = genome_metadata["collection_date"]
-        geo_loc_name = genome_metadata["geo_loc_name"]
+    metadata_df = pd.read_csv(
+        metadata_path,
+        usecols=["accession", "collection_date", "geo_loc_name"],
+    ).set_index("accession")
 
-        ## string matching for now - should switch to something more robust. 
-        left_fwd = [m.start() for m in re.finditer(pattern_left,
-                                                   genome_seq,
-                                                   flags=re.IGNORECASE,
-                                                   overlapped=True)]
-        right_rev = [m.start() for m in re.finditer(pattern_right,
+    all_results = []
+
+    for _, row in primer_df.iterrows():
+        primer_left = row["primer_seq_x"]
+        primer_right = row["primer_seq_y"]
+        primer_right = str(Seq(primer_right).reverse_complement())
+        
+        pattern_left = f"({primer_left}){{s<={maxmismatch}}}"
+        pattern_right = f"({primer_right}){{s<={maxmismatch}}}"
+
+        # # Parse sequences in the multifasta
+        for genome_record in SeqIO.parse(genomes, "fasta"):
+            genome_id = genome_record.id
+            genome_seq = str(genome_record.seq)
+            genome_metadata = metadata_df.loc[genome_id]
+            collection_date = genome_metadata["collection_date"]
+            geo_loc_name = genome_metadata["geo_loc_name"]
+
+            ## string matching for now - should switch to something more robust. 
+            left_fwd = [m.start() for m in re.finditer(pattern_left,
                                                     genome_seq,
                                                     flags=re.IGNORECASE,
                                                     overlapped=True)]
-        if len(left_fwd)>1 or len(right_rev)>1:
-            print('multiple matching sites!')
-            asdfasdf
-        left_fwd_actual = [genome_seq[pos:pos+len(primer_left)]
-                           for pos in left_fwd]
-        right_rev_actual = [genome_seq[pos:pos+len(primer_right)]
-                            for pos in right_rev]
-        distance_left = [edit_distance(actual, primer_left)
-                         for actual in left_fwd_actual]
-        distance_right = [edit_distance(actual, primer_right)
-                          for actual in right_rev_actual]
+            right_rev = [m.start() for m in re.finditer(pattern_right,
+                                                        genome_seq,
+                                                        flags=re.IGNORECASE,
+                                                        overlapped=True)]
+            if len(left_fwd)>1 or len(right_rev)>1:
+                print('multiple matching sites!')
+                asdfasdf
+            left_fwd_actual = [genome_seq[pos:pos+len(primer_left)]
+                            for pos in left_fwd]
+            right_rev_actual = [genome_seq[pos:pos+len(primer_right)]
+                                for pos in right_rev]
+            distance_left = [edit_distance(actual, primer_left)
+                            for actual in left_fwd_actual]
+            distance_right = [edit_distance(actual, primer_right)
+                            for actual in right_rev_actual]
 
-        left_has_ambiguity = [any(base in ambiguous_bases
-                                  for base in lfa.upper())
-                              for lfa in left_fwd_actual]
-        right_has_ambiguity = [any(base in ambiguous_bases
-                                   for base in rfa.upper())
-                               for rfa in right_rev_actual]
-        for jL,lfa in enumerate(left_fwd_actual):
-            for jR,rfa in enumerate(right_rev_actual):
-                all_results.append([genome_id,collection_date,geo_loc_name,primer_left,primer_right,lfa,rfa,distance_left[jL],left_has_ambiguity[jL],distance_right[jR],right_has_ambiguity[jR]])
+            left_has_ambiguity = [any(base in ambiguous_bases
+                                    for base in lfa.upper())
+                                for lfa in left_fwd_actual]
+            right_has_ambiguity = [any(base in ambiguous_bases
+                                    for base in rfa.upper())
+                                for rfa in right_rev_actual]
+            for jL,lfa in enumerate(left_fwd_actual):
+                for jR,rfa in enumerate(right_rev_actual):
+                    all_results.append([genome_id,collection_date,geo_loc_name,primer_left,primer_right,lfa,rfa,distance_left[jL],left_has_ambiguity[jL],distance_right[jR],right_has_ambiguity[jR]])
 
-result_df = pd.DataFrame(all_results, columns=['sequence','collection_date','geo_loc_name','primer_left','primer_right_rev_comp','left_match','right_rev_comp_match','edit_dist_left','left_amb_bases','edit_dist_right','right_amb_bases'])
+    result_df = pd.DataFrame(all_results, columns=['sequence','collection_date','geo_loc_name','primer_left','primer_right_rev_comp','left_match','right_rev_comp_match','edit_dist_left','left_amb_bases','edit_dist_right','right_amb_bases'])
 
-left_dist_min = result_df.groupby('sequence')['edit_dist_left'].min()
-right_dist_min = result_df.groupby('sequence')['edit_dist_right'].min()
+    left_dist_min = result_df.groupby('sequence')['edit_dist_left'].min()
+    right_dist_min = result_df.groupby('sequence')['edit_dist_right'].min()
+    left_result_mins = result_df.loc[result_df.groupby('sequence')['edit_dist_left'].idxmin()]
+    right_result_mins = result_df.loc[result_df.groupby('sequence')['edit_dist_right'].idxmin()]
 
-print('Minimum edit distances (all sequences), for left/right primers')
-print(left_dist_min.value_counts().sort_index())
-print(right_dist_min.value_counts().sort_index())
+    if primer_info[virus]['assay']==1:
+        left_result_mins = left_result_mins.to_csv(f'../primer_scoring/left_mins_{virus}.csv')
+        right_result_mins = right_result_mins.to_csv(f'../primer_scoring/right_mins_{virus}.csv')
+    else:
+        left_result_mins = left_result_mins.to_csv(f"../primer_scoring/left_mins_{virus}_{primer_info[virus]['assay']}.csv")
+        right_result_mins = right_result_mins.to_csv(f"../primer_scoring/right_mins_{virus}_{primer_info[virus]['assay']}.csv")
+    
+    print('Minimum edit distances (all sequences), for left/right primers')
+    print(left_dist_min.value_counts().sort_index())
+    print(right_dist_min.value_counts().sort_index())
 
-result_df = result_df[result_df['collection_date'].str.contains('2024')| 
-                        result_df['collection_date'].str.contains('2025')| 
-                        result_df['collection_date'].str.contains('2026')]
-left_dist_min = result_df.groupby('sequence')['edit_dist_left'].min()
-right_dist_min = result_df.groupby('sequence')['edit_dist_right'].min()
+    result_df = result_df[(result_df['collection_date'].str.contains('2024')| 
+                            result_df['collection_date'].str.contains('2025')| 
+                            result_df['collection_date'].str.contains('2026')) & (result_df['geo_loc_name'].str.contains("USA"))]
+    left_dist_min = result_df.groupby('sequence')['edit_dist_left'].min()
+    right_dist_min = result_df.groupby('sequence')['edit_dist_right'].min()
 
-print('Minimum edit distances (sequences from last two years), for left/right primers')
-print(left_dist_min.value_counts().sort_index())
-print(right_dist_min.value_counts().sort_index())
+    print('Minimum edit distances (sequences from last two years), for left/right primers')
+    print(left_dist_min.value_counts().sort_index())
+    print(right_dist_min.value_counts().sort_index())
 
+    probe = primer_info[virus]['probe']
 
-probe_results = []
-### now do the same for the probe
-probe_df = pd.DataFrame([probe_v for probe_v in expand_primer_options(probe, translator)], columns=["probe_seq"])
-# # Parse sequences in the multifasta
-for _, row in probe_df.iterrows():
-    probe_seq = row["probe_seq"]
-    # primer_right = row["primer_seq_y"]
-    # primer_right = str(Seq(primer_right).reverse_complement())
-    pattern_probe = f"({probe_seq}){{s<={maxmismatch}}}"
-    # pattern_right = f"({primer_right}){{s<={maxmismatch}}}"
-    for genome_record in SeqIO.parse(genomes, "fasta"):
-        genome_id = genome_record.id
-        genome_seq = str(genome_record.seq)
-        genome_metadata = metadata_df.loc[genome_id]
-        collection_date = genome_metadata["collection_date"]
-        geo_loc_name = genome_metadata["geo_loc_name"]
+    probe_results = []
+    ### now do the same for the probe
+    probe_df = pd.DataFrame([probe_v for probe_v in expand_primer_options(probe, translator)], columns=["probe_seq"])
+    # # Parse sequences in the multifasta
+    for _, row in probe_df.iterrows():
+        probe_seq = row["probe_seq"]
+        # primer_right = row["primer_seq_y"]
+        # primer_right = str(Seq(primer_right).reverse_complement())
+        pattern_probe = f"({probe_seq}){{s<={maxmismatch}}}"
+        # pattern_right = f"({primer_right}){{s<={maxmismatch}}}"
+        for genome_record in SeqIO.parse(genomes, "fasta"):
+            genome_id = genome_record.id
+            genome_seq = str(genome_record.seq)
+            genome_metadata = metadata_df.loc[genome_id]
+            collection_date = genome_metadata["collection_date"]
+            geo_loc_name = genome_metadata["geo_loc_name"]
 
-        ## string matching for now - should switch to something more robust. 
-        probe_fwd = [m.start() for m in re.finditer(pattern_probe,
-                                                   genome_seq,
-                                                   flags=re.IGNORECASE,
-                                                   overlapped=True)]
-        if len(probe_fwd)>1 :
-            print('multiple matching sites!')
-            asdfasdf
-        probe_fwd_actual = [genome_seq[pos:pos+len(probe)]
-                           for pos in probe_fwd]
-        distance_probe = [edit_distance(actual, probe_seq)
-                         for actual in probe_fwd_actual]
+            ## string matching for now - should switch to something more robust. 
+            probe_fwd = [m.start() for m in re.finditer(pattern_probe,
+                                                    genome_seq,
+                                                    flags=re.IGNORECASE,
+                                                    overlapped=True)]
+            if len(probe_fwd)>1 :
+                print('multiple matching sites!')
+                asdfasdf
+            probe_fwd_actual = [genome_seq[pos:pos+len(probe)]
+                            for pos in probe_fwd]
+            distance_probe = [edit_distance(actual, probe_seq)
+                            for actual in probe_fwd_actual]
 
-        site_has_ambiguity = [any(base in ambiguous_bases
-                                  for base in lfa.upper())
-                              for lfa in probe_fwd_actual]
-        for jL,pfa in enumerate(probe_fwd_actual):
-            probe_results.append([genome_id,collection_date,geo_loc_name,probe_seq,pfa,distance_probe[jL],site_has_ambiguity[jL]])
-probe_result_df = pd.DataFrame(probe_results, columns=['sequence','collection_date','geo_loc_name','probe_seq','probe_match','edit_distance_probe','probe_amb_bases'])
-probe_dist_min = probe_result_df.groupby('sequence')['edit_distance_probe'].min()
+            site_has_ambiguity = [any(base in ambiguous_bases
+                                    for base in lfa.upper())
+                                for lfa in probe_fwd_actual]
+            for jL,pfa in enumerate(probe_fwd_actual):
+                probe_results.append([genome_id,collection_date,geo_loc_name,probe_seq,pfa,distance_probe[jL],site_has_ambiguity[jL]])
+    probe_result_df = pd.DataFrame(probe_results, columns=['sequence','collection_date','geo_loc_name','probe_seq','probe_match','edit_distance_probe','probe_amb_bases'])
+    probe_dist_min = probe_result_df.groupby('sequence')['edit_distance_probe'].min()
+    probe_result_mins = probe_result_df.loc[probe_result_df.groupby('sequence')['edit_distance_probe'].idxmin()]
+    if primer_info[virus]['assay']==1:
+        probe_result_mins = probe_result_mins.to_csv(f'../primer_scoring/probe_mins_{virus}.csv')
+    else:
+        probe_result_mins = probe_result_mins.to_csv(f"../primer_scoring/probe_mins_{virus}_{primer_info[virus]['assay']}.csv")
+    print('Minimum edit distances (all seqs), for probe')
+    print(probe_dist_min.value_counts().sort_index())
 
-print('Minimum edit distances (all seqs), for probe')
-print(probe_dist_min.value_counts().sort_index())
+    probe_result_df = probe_result_df[(probe_result_df['collection_date'].str.contains('2024')| 
+                            probe_result_df['collection_date'].str.contains('2025')| 
+                            probe_result_df['collection_date'].str.contains('2026')) & (probe_result_df['geo_loc_name'].str.contains("USA"))]
+    probe_dist_min = probe_result_df.groupby('sequence')['edit_distance_probe'].min()
 
-probe_result_df = probe_result_df[probe_result_df['collection_date'].str.contains('2024')| 
-                        probe_result_df['collection_date'].str.contains('2025')| 
-                        probe_result_df['collection_date'].str.contains('2026')]
-probe_dist_min = probe_result_df.groupby('sequence')['edit_distance_probe'].min()
-
-print('Minimum edit distances (sequences from last two years), for probe')
-print(probe_dist_min.value_counts().sort_index())
+    print('Minimum edit distances (sequences from last two years), for probe')
+    print(probe_dist_min.value_counts().sort_index())
